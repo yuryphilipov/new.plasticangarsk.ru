9afca20f7c4c7c7cdd6d275ca41ae85657df78ff
Yuriy Philipov <yury.philipov@gmail.com>
2021-03-10 14:29:36 +0800

    gulp configred

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,70 @@
-const { src, dest, watch, parallel } = require("gulp");
-const css = require("gulp-sass");
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass");
+const prefixer = require("gulp-autoprefixer");
+const gcmq = require("gulp-group-css-media-queries");
 const fileinclude = require("gulp-file-include");
+const browserSync = require("browser-sync").create();
+const del = require("del");
 
-function sass() {
-  return src("scss/**/*.scss")
-    .pipe(css())
-    .pipe(dest("css"));
+function browser() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
 }
 
-function include() {
-  return src(["./html/index.html"])
-    .pipe(
-      fileinclude({
-        prefix: "@@",
-        basepath: "@file"
-      })
-    )
-    .pipe(dest("./"));
+function css() {
+  return src(["./src/scss/**/*.scss", "!./src/scss/**/_*.scss"])
+    .pipe(sass())
+    .pipe(prefixer({ cascade: false }))
+    .pipe(gcmq())
+    .pipe(dest("./dist/css"));
+}
+
+function html() {
+  return src(["./src/html/**/*.html", "!./src/html/**/_*.html"])
+    .pipe(fileinclude())
+    .pipe(dest("./dist"));
+}
+
+function img() {
+  return src(["./src/img/**/*.{jpg,png,svg,ico,webp}"]).pipe(
+    dest("./dist/img")
+  );
+}
+
+function js() {
+  return src(["./src/js/**/*.js"]).pipe(dest("./dist/js"));
+}
+
+function plugins() {
+  return src(["./src/plugins/**/*"]).pipe(dest("./dist/plugins"));
+}
+
+function clean() {
+  return del("dist");
 }
 
 function _watch() {
-  watch("scss/**/*.scss", sass);
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+
+  watch("src/html/**/*.html", html).on("change", browserSync.reload);
+  watch("src/scss/**/*.scss", css).on("change", browserSync.reload);
+  watch("src/js/**/*.js", js).on("change", browserSync.reload);
+  watch("src/img/**/*", img).on("change", browserSync.reload);
 }
 
-exports.watch = _watch;
-exports.include = include;
+exports.browser = browser;
+exports.html = html;
+exports.css = css;
+exports.img = img;
+exports.js = js;
+exports.plugins = plugins;
+
+exports.build = series(clean, css, img, js, plugins, html);
+exports.serve = series(clean, css, img, js, plugins, html, _watch);
